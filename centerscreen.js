0bff8a1a7c077148b9ba06c54149d600826d79dd
LeAnnAlexandra <leannalexandraviolet@gmail.com>
2023-07-26 13:34:49 +0200

    Tinkered to Perfection: Optimised projects

--- centerscreen.js
+++ centerscreen.js
@@ -1,13 +1,13 @@
-///gather array of all divs with .wrap class
-const previewableProjects = document.getElementsByClassName("wrap");
+// ///gather array of all divs with .wrap class
+// const previewableProjects = document.getElementsByClassName("wrap");
 
-//iterate through all add a event listener for mouse enter ()
+// //iterate through all add a event listener for mouse enter ()
 
-for(let wrap of previewableProjects){
-    wrap.addEventListener("mouseover",()=>{
-        return;
-        console.log("this is div");
+// for(let wrap of previewableProjects){
+//     wrap.addEventListener("mouseover",()=>{
+//         return;
+//         console.log("this is div");
 
 
-    });
-}
+//     });
+// }
